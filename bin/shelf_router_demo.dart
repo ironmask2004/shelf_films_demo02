d0fbde52be4eade6c7f8670591766219f46bc77c
ironmask2004 <ironmask2004@gmail.com>
2021-08-26 10:58:35 +0300

    first commit
    first commit
    
    andro1
    
    andro1
    
    andro1
    
    homelap
    
    22
    
    22
    
    Update shelf_router_demo.dart
    
    33

---
+++ bin/shelf_router_demo.dart
@@ -0,0 +1,19 @@
+import 'package:shelf_router/shelf_router.dart';
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart' as io;
+
+import 'package:shelf_router_demo/film_api.dart';
+
+void main(List<String> arguments) async {
+  final app = Router();
+
+  app.mount('/films/', FilmApi().router);
+
+  app.get('/<name|.*>', (Request request, String name) {
+    final param = name.isNotEmpty ? name : 'World!!!!';
+    return Response.ok('Hello $param!');
+  });
+
+  print('working server try:  curl  localhost:8083/films/2 ');
+  await io.serve(app, 'localhost', 8083);
+}
