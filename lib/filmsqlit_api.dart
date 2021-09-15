a4431ecf67089d058fba19665c21b4bcd6f4e516
ironmask2004 <ironmask2004@gmail.com>
2021-09-15 11:37:01 +0300

    sqlit3 lib

--- lib/filmsqlit_api.dart
+++ lib/filmsqlit_api.dart
@@ -1,17 +1,20 @@
+import 'dart:ffi';
 import 'dart:io';
 import 'dart:convert';
 import 'package:shelf_router/shelf_router.dart';
 import 'package:shelf/shelf.dart';
 import 'dart:async';
-
-//import 'package:path/path.dart';
+import 'package:path/path.dart';
+import 'package:sqlite3/open.dart';
 import 'package:sqlite3/sqlite3.dart';
+
+import 'film_class.dart';
 
 //import 'film_class.dart';
 
 class FilmSqlApi {
   final List data = json.decode(File('films.json').readAsStringSync());
- // late final database ;
+  late final database ;
   FilmSqlApi() {
     print('------------');
     print('Films Sql Data');
@@ -22,8 +25,50 @@ class FilmSqlApi {
     print(data[0]['title']);
     //print(data[1]['title']);
     print('------------');
-   // open_filmsDB();
+
+
+    open_filmsDB();
+    print('Using sqlite3 ${sqlite3.version}');
   }
+
+  open_filmsDB(){
+
+    open.overrideFor(OperatingSystem.linux, _openOnLinux);
+    // After setting all the overrides, you can use moor!
+    database = sqlite3.openInMemory();
+    var stmt = database.prepare('CREATE TABLE film(id INTEGER PRIMARY KEY, title TEXT)' );
+    stmt.execute();
+    print('created table film');
+    // Dispose a statement when you don't need it anymore to clean up resources.
+    stmt.dispose();
+
+    stmt = database.prepare('INSERT INTO film (id, title) VALUES (?,?)');
+    stmt.execute([1, 'The Beatles1']);
+    stmt.execute([2, 'The Beatles2']);
+    stmt.execute([3, 'The Beatles3']);
+
+    // Dispose a statement when you don't need it anymore to clean up resources.
+    stmt.dispose();
+
+
+//    final ResultSet resultSet = database.select('SELECT * FROM film WHERE name LIKE ?', ['The %']);
+    final ResultSet resultSet =database.select('SELECT * FROM film ');
+    resultSet.forEach((element) {
+      print(element);
+    });
+    for (final Row row in resultSet) {
+      print('last rec : Film [id: ${row['id']}, title: ${row['title']}]');
+    }
+
+  }
+
+  DynamicLibrary _openOnLinux() {
+    final scriptDir = File(Platform.script.toFilePath()).parent;
+    print(scriptDir);
+    final libraryNextToScript = File('${scriptDir.path}/libsqlite3.so');
+    return DynamicLibrary.open(libraryNextToScript.path);
+  }
+
 
   Router get router {
     final router = Router();
@@ -48,12 +93,12 @@ class FilmSqlApi {
     router.post('/', (Request request) async {
       final payload = await request.readAsString();
       data.add(json.decode(payload));
-     /* var film0 = Film(
+     var film0 = Film(
         id:await jsonDecode(payload)['id'] ,
          title: await jsonDecode(payload)['title']
       );
-      await insertFilm(film0);
-      */
+      // await insertFilm(film0);
+
       return Response.ok(payload,
           headers: {'Content-Type': 'application/json'});
     });
